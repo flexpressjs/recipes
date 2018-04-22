e15851906c6903e0565c86d8fd78c0248222fd1d
Titouan Galopin <galopintitouan@gmail.com>
2018-04-22 15:54:34 -0700

    Add installation instructions for express

--- express/4.0.0/src/controllers/welcome.js
+++ express/4.0.0/src/controllers/welcome.js
@@ -1,5 +1,8 @@
 module.exports = (app) => {
     return (req, res) => {
-        res.send('Welcome on Flexpress!<br /><a href="">Read the documentation</a> to learn more about what to do next!');
+        res.send(
+            'Welcome on Flexpress!<br />' +
+            '<a href="https://github.com/flexpressjs/flexpress">Read the documentation</a> to learn more about what to do next!'
+        );
     };
 };
